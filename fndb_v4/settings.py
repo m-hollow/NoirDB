76cd4c4bab56924bd7ccf17025d3ce2a109526af
m-hollow <mholloway@audiophonic.com>
2020-08-08 14:48:30 -0700

    sk removed

--- fndb_v4/settings.py
+++ fndb_v4/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'r9aj(u&4=9a=r6-hmh(kz2jybg8yhn@#d44u$nk&r&g5#i(u8e'
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
